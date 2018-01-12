4bcab51bbd98497a4491114bc824176f562df838
Coumes Quentin <qcoumes@etud.u-pem.fr>
2018-01-12 16:17:57 +0100

    Ajout des logs

--- pl_sandbox/settings.py
+++ pl_sandbox/settings.py
@@ -19,10 +19,6 @@ from pl_sandbox.testing import DatabaselessTestRunner
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'oqa57as*^hu4()mtg7r*t^r_9d&g5$ynsbzz63wf&b6y^vbea6'
 
@@ -33,7 +29,6 @@ ALLOWED_HOSTS = ['127.0.0.1', 'pl-sandbox-test.u-pem.fr']
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'sandbox',
     'django.contrib.admin',
@@ -52,37 +47,85 @@ WSGI_APPLICATION = 'pl_sandbox.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {}
 
 # Needed for manage.py to run without database
 TEST_RUNNER = 'pl_sandbox.testing.DatabaselessTestRunner'
 
-
 # Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = []
+
+#Write email in console instead of sending it if DEBUG is set to True
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#Logger information
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)-15s] %(levelname)s -- File: %(pathname)s line n°%(lineno)d -- %(message)s',
+            'datefmt': '%Y/%m/%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '[%(asctime)-15s] %(levelname)s -- %(message)s',
+            'datefmt': '%Y/%m/%d %H:%M:%S'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'syslog': {
+            'level': 'INFO',
+            'class': 'logging.handlers.SysLogHandler',
+            'facility': 'local6',
+            'address': '/dev/log',
+            'formatter': 'verbose'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django':{
+            'handlers': ['console', 'syslog', 'mail_admins'],
+            'level': 'INFO',
+        },
+        'sandbox':{
+            'handlers': ['console', 'syslog', 'mail_admins'],
+            'level': 'INFO',
+        },
+    },
+}
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '../tmp')
@@ -94,10 +137,10 @@ image = "default:PL"
 environment = {}        #(dict) Environment variables to set inside the container, as a dictionary.
 mem_limit = "500m"      #(str) Memory limit. String with a units identification char (13b, 12k, 14m, 1g).
 memswap_limit = 0       #(str) See https://docs.docker.com/engine/admin/resource_constraints/#--memory-swap-details
-cpuset_cpus = "0"     #(str) CPUs in which to allow execution ("0-3", "0,1").
+cpuset_cpus = "0"       #(str) CPUs in which to allow execution ("0-3", "0,1").
 
 
-# Docker creating function
+# Docker creation function
 def CREATE_DOCKER():
     return docker.from_env().containers.run(
         image,
--- sandbox/views.py
+++ sandbox/views.py
@@ -7,7 +7,7 @@
 #  Last Modified: 2017-09-30
 
 
-import os, time, shutil
+import os, time, shutil, logging
 
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
@@ -15,24 +15,33 @@ from django.http import HttpResponse, Http404
 
 from sandbox.executor import Executor
 
+logger = logging.getLogger(__name__)
 
 
 @csrf_exempt
 def execute(request):
     if request.META["REQUEST_METHOD"] == "HEAD":
+        logger.info("HEAD request received from '"+request.META['REMOTE_ADDR']+"'");
         return HttpResponse('OK !', status=200)
     if request.META["REQUEST_METHOD"] != "POST":
+        logger.warning("Invalid methode ("+request.META["REQUEST_METHOD"]+") received from '"+request.META['REMOTE_ADDR']+"'");
         return HttpResponse('405 Method Not Allowed', status=405)
     
     # Removing tmp files older than 2 hours
     current_time = time.time()
     for f in os.listdir(settings.MEDIA_ROOT):
-        creation_time = os.path.getctime(f)
+        if "README" in f:
+            continue
+        creation_time = os.path.getctime(settings.MEDIA_ROOT+"/"+f)
         if (current_time - creation_time) >= 7200:
-            shutil.rmtree(f)
-    
-    return HttpResponse(Executor(request).execute())
-
+            shutil.rmtree(settings.MEDIA_ROOT+"/"+f)
+            
+    logger.info("Sandbox request received from '"+request.META['REMOTE_ADDR']+"'");
+    try:
+        return HttpResponse(Executor(request).execute())
+    except: 
+        logger.exception("Unknown Error:")
+        return HttpResponse('520 Unknown Error', status=520)
 
 @csrf_exempt
 def action(request):
