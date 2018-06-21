cd945fd4881877aa87186331eacca1ed8c0494d6
root <root@pl-test.u-pem.fr>
2018-06-21 11:43:45 +0200

    Restarting docker after build

--- sandbox/docker/build.sh
+++ sandbox/docker/build.sh
@@ -1,3 +1,4 @@
 docker ps -a | awk '{ print $1,$2 }' | grep pl:base | awk '{print $1 }' | xargs -I {} docker stop {} > /dev/null
 docker rmi -f pl:base || true
 docker build -t pl:base .
+systemctl restart docker
