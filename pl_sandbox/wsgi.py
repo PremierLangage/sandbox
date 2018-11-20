918d7779fcc9ee430a442edd86c69b50d1d5cc4e
pl-test <root@pl-test.u-pem.fr>
2018-11-20 10:56:49 +0100

    fix requirements and wsgi

--- pl_sandbox/wsgi.py
+++ pl_sandbox/wsgi.py
@@ -12,6 +12,7 @@ import os, sys
 from django.core.wsgi import get_wsgi_application
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 if path not in sys.path:
     sys.path.append(path
 
