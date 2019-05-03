199f1717b87f12b92a9fb50ee91ca929d3ae9091
plgitlogin <plgitlogin@users.noreply.github.com>
2019-05-03 16:08:27 +0200

    Removed docker restart at the end of build.sh

--- sandbox/docker/build.sh
+++ sandbox/docker/build.sh
@@ -2,4 +2,3 @@
 docker ps -a | awk '{ print $1,$2 }' | grep pl:latest | awk '{print $1 }' | xargs -I {} docker stop {} > /dev/null
 docker rmi -f pl:latest || true
 docker build -t pl:latest .
-systemctl restart docker
