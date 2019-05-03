b1d7c9ecc5308f7c6d82f6c5054442f673c8335d
Coumes Quentin <qcoumes@etud.u-pem.fr>
2019-05-03 12:09:30 +0200

    Renaming image to pl:latest

--- sandbox/docker/build.sh
+++ sandbox/docker/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker ps -a | awk '{ print $1,$2 }' | grep pl:base | awk '{print $1 }' | xargs -I {} docker stop {} > /dev/null
-docker rmi -f pl:base || true
-docker build -t pl:base .
+docker ps -a | awk '{ print $1,$2 }' | grep pl:latest | awk '{print $1 }' | xargs -I {} docker stop {} > /dev/null
+docker rmi -f pl:latest || true
+docker build -t pl:latest .
 systemctl restart docker
