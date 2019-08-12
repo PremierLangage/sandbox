62e1649b99c33b6d4dfc777c0642b6d6467fd343
Quentin Coumes <coumes.quentin@gmail.com>
2019-08-12 18:48:29 +0200

    Added commands and g++ to /libraries/

--- docker/libraries.py
+++ docker/libraries.py
@@ -1,16 +1,21 @@
 import json
 import os
+import stat
 import subprocess
 import sys
 
 
 devnull = open(os.devnull, "w")
 
-python2 = subprocess.check_output(["/usr/bin/python2", "--version"], stderr=subprocess.STDOUT).decode().split()[1].strip()
+python2 = subprocess.check_output(["/usr/bin/python2", "--version"],
+                                  stderr=subprocess.STDOUT).decode().split()[1].strip()
 python3 = ".".join(map(str, sys.version_info[:3]))
-java = " ".join(subprocess.check_output(["/usr/bin/java", "--version"]).decode().split("\n")[0].split()[:2])
+java = " ".join(
+    subprocess.check_output(["/usr/bin/java", "--version"]).decode().split("\n")[0].split()[:2])
 gcc = subprocess.check_output(["/usr/bin/gcc", "--version"]).decode().split("\n")[0].split()[3]
-perl = subprocess.check_output(["/usr/bin/perl", "--version"]).decode().split("\n")[1].split()[8][2:-1]
+gpp = subprocess.check_output(["/usr/bin/g++", "--version"]).decode().split("\n")[0].split()[3]
+perl = subprocess.check_output(["/usr/bin/perl", "--version"]).decode().split("\n")[1].split()[8][
+       2:-1]
 postgres = subprocess.check_output(["/usr/bin/psql", "--version"]).decode().split()[2]
 
 system_packages = dict()
@@ -21,7 +26,8 @@ for line in subprocess.check_output(commands, stderr=devnull).decode().split("\n
     system_packages[lib] = version
 
 python_modules = dict()
-for line in subprocess.check_output(["/usr/local/bin/pip", "freeze"], stderr=devnull).decode().split("\n")[:-1]:
+for line in subprocess.check_output(["/usr/local/bin/pip", "freeze"],
+                                    stderr=devnull).decode().split("\n")[:-1]:
     lib, version = line.split("==")
     python_modules[lib] = version
 
@@ -38,23 +44,34 @@ for line in lines:
 perl_modules = dict()
 commands = ["/bin/bash", "-c", "yes | /usr/bin/cpan -l"]
 subprocess.check_output(commands, stderr=devnull)
-for line in subprocess.check_output(["/usr/bin/cpan", "-l"], stderr=devnull).decode().split("\n")[1:-1]:
+for line in subprocess.check_output(["/usr/bin/cpan", "-l"], stderr=devnull).decode().split("\n")[
+            1:-1]:
     lib, version = line.split("\t")
     if version == "undef":
         version = "?"
     perl_modules[lib] = version
 
+bins = []
+executable = stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH
+for path in os.environ["PATH"].split(":"):
+    for name in os.listdir(path):
+        file_path = os.path.join(path, name)
+        if os.path.isfile(file_path) and os.stat(file_path).st_mode & executable:
+            bins.append(name)
+
 print(json.dumps({
-    "python3":    python3,
-    "python2":    python2,
-    "java":       java,
-    "gcc":        gcc,
-    "perl":       perl,
-    "postgres":   postgres,
-    "librairies": {
-        "system": system_packages,
-        "python": python_modules,
-        "perl":   perl_modules,
-        "c":      c_libs,
-    }
+    "python3":   python3,
+    "python2":   python2,
+    "java":      java,
+    "gcc":       gcc,
+    "g++":       gpp,
+    "perl":      perl,
+    "postgres":  postgres,
+    "libraries": {
+        "system": dict(sorted(system_packages.items())),
+        "python": dict(sorted(python_modules.items())),
+        "perl":   dict(sorted(perl_modules.items())),
+        "c":      dict(sorted(c_libs.items())),
+    },
+    "bin":       sorted(bins),
 }))
