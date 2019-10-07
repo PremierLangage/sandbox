b6a5c4129bf8536304817cc83a0b7835a4c2ef65
Quentin Coumes <qcoumes@etud.u-pem>
2019-10-07 11:45:56 +0200

    Added wget on Francis' DB in the Dockerfile

--- sandbox/docker/buildSQL.sh
+++
@@ -1,6 +0,0 @@
-
-#!/usr/bin/env bash
-docker ps -a | awk '{ print $1,$2 }' | grep pl:base | awk '{print $1 }' | xargs -I {} docker stop {} > /dev/null
-docker rmi -f pl:base || true
-docker build -file DockerfileSQL -t pl:sql .
-systemctl restart docker
