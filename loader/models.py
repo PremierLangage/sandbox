d6e585a03df80a189e12e8f0538e931186104019
Killian Blanchard <kblanc01@etud.u-pem.fr>
2021-05-31 14:01:58 +0200

    refactor: :recycle: update frozenresourcce model

--- loader/models.py
+++ loader/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class FrozenResource(models.Model):
     hash = models.CharField(primary_key=True ,max_length=100)
     data = models.JSONField(default=dict)
-    parent = models.ManyToManyField("self", related_name="frs")
+    parent = models.ManyToManyField("self", symmetrical=False)
 
+
+   
