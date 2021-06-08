f61afb744060d0a8a84c6fd18d7da348d78c7a0b
Killian Blanchard <kblanc01@etud.u-pem.fr>
2021-06-08 10:50:11 +0200

    refactor: :sparkles: add new url to play_exo

--- api_server/urls.py
+++ api_server/urls.py
@@ -7,5 +7,6 @@ app_name = "api_server"
 urlpatterns = [
    path('loader/fr/<int:id>/', views.FrozenViewSet.as_view({'get':'get'}), name='frozen_get'),
    path('loader/fr/', views.FrozenViewSet.as_view({'post':'post'}), name='frozen_post'),
-   path('loader/demo/pl/', views.CallSandboxViewSet.as_view({'post':'play_demo'}), name='play_demo')
+   path('loader/demo/pl/', views.CallSandboxViewSet.as_view({'post':'play_demo'}), name='play_demo'),
+   path('loader/play/pl/', views.CallSandboxViewSet.as_view({'post':'play_exo'}), name='play_exo'),
 ]
