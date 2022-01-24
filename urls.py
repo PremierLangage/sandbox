9edba77a1a8a7fe9a9fe77544bf0ebfe36d441a0
BrotherKey <bkey@bkey-compagnie.local>
2022-01-24 10:34:04 +0100

    feat: file loader

--- urls.py
+++ urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^', include('sandbox.urls')),
+    path('', include('sandbox.urls', namespace='sandbox')),
+    path('', include('sandbox_api.urls', namespace='sandbox_api')),
 ]
