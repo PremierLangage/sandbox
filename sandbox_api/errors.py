b0e100e9817d5476ce78ba9d92d4b0140a04260e
BrotherKey <bkey@bkey-compagnie.local>
2022-01-31 10:25:26 +0100

    feat: sandbox loader

--- sandbox_api/errors.py
+++ sandbox_api/errors.py
@@ -1,18 +1,65 @@
+import json
+from django.http import HttpResponse
 
-class LoaderInstanceError(Exception):
-    """Raised when type of files not expected."""
-
-    def __init__(self, message: str, code: int) -> None:
-        self.message = message
-        self.code = code
+class WarningError(Exception):
+    def __init__(self, title: str, detail: str) -> None:
+        self.title = title
+        self.detail = detail
 
     def __repr__(self) -> str:
-        return f'"code":{self.code}, "message"="{self.message}"'
+        return self.__str__()
 
     def __str__(self) -> str:
-        return """
-        {
-            "code":{self.code},
-            "message":{self.message}
-        }
-        """
+        return "{" + f'"title":{self.title}, "detail":{self.details}' + "}"
+    
+    def dict(self) -> dict:
+        return {"title": self.title, "detail": self.detail}
+class FatalError(Exception):
+    """Default handelable error."""
+    def __init__(self, message: str, details: list, code: int) -> None:
+        self.message = message
+        self.code = code
+        self.details = details
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __str__(self) -> str:
+        return f'"message":{self.message}, "status":{self.code}'
+    
+    def __dict__(self) -> dict:
+        context = {}
+        context["message"] = self.message
+        context["status"] = self.code
+        if self.details:
+            context["details"] = self.details
+        return context
+
+    def response(self) -> HttpResponse:
+        return HttpResponse(
+            json.dumps(self.__dict__()),
+            status = self.code,
+            content_type="application/json")
+    
+class LoaderError(FatalError):
+    def __init__(self, message: str, details: list) -> None:
+        super().__init__("Loader error : " + message, details, 500)
+class LoaderInstanceError(FatalError):
+    def __init__(self, details: str) -> None:
+        super().__init__("Loader instance error", details, 400)
+
+class LoaderContextError(FatalError):
+    def __init__(self, details: str) -> None:
+        super().__init__("Malformed context for Loader", details, 400)
+
+class LoaderSandboxError(FatalError):
+    def __init__(self, details: str) -> None:
+        super().__init__("Sandbox error", details, 500)
+
+class LoaderIncludeError(FatalError):
+    def __init__(self, details: str) -> None:
+        super().__init__("Loader file error", details, 500)
+
+class MissingFile(WarningError):
+    def __init__(self, detail: str) -> None:
+        super().__init__("Missing file", detail)
